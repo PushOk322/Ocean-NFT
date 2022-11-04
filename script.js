691c977aa2001b14522e9ea62e486eb8cfa989e3
Push_Ok <79057320+PushOk322@users.noreply.github.com>
2022-11-04 17:37:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const iconMenu = document.querySelector('.menu-icon');
+if (iconMenu) {
+    const menuLinks = document.querySelector('.header__links');
+    const menuSign = document.querySelector('.header__right');
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuLinks.classList.toggle('_active');
+        menuSign.classList.toggle('_active');
+    });
+}
+
+const accordeonButtons = document.querySelectorAll('.footer__panel-heading');
+if (accordeonButtons) {
+    const arrow = document.querySelectorAll('.footer__arrow');
+    const panel = document.querySelectorAll('.footer__panel');
+    for (let i = 0; i < accordeonButtons.length; i++) {
+        accordeonButtons[i].addEventListener("click", function (e) {
+            if (arrow[i].classList.contains('active')) {
+                for (let j = 0; j < accordeonButtons.length; j++) {
+                    arrow[j].classList.add('active');
+                    panel[j].classList.add('active');
+                };
+                arrow[i].classList.remove('active');
+                panel[i].classList.remove('active');
+                console.log(1);
+            } else {
+                for (let j = 0; j < accordeonButtons.length; j++) {
+                    arrow[j].classList.add('active');
+                    panel[j].classList.add('active');
+                };
+                arrow[i].classList.add('active');
+                panel[i].classList.add('active');
+            }
+        })
+    };
+}
+new Swiper('.swiper-container', {
+    breakpoints: {
+        1370: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+        },
+
+        1024: {
+            slidesPerView: 2.2,
+            spaceBetween: 25,
+        },
+
+        653: {
+            slidesPerView: 1.1,
+            spaceBetween: 15,
+        },
+
+        320: {
+            slidesPerView: 1.1,
+            spaceBetween: 10,
+        }
+    }
+});
